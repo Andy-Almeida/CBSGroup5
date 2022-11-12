72f74e4f493a708bcd5a88e41bf265f1237365c6
Kao Saephan <112367295+ksaephan@users.noreply.github.com>
2022-11-12 13:35:10 -0800

    Update basketball_team.h

--- basketball_team.h
+++ basketball_team.h
@@ -12,18 +12,27 @@ namespace BASKETBALL_SPACE{
   
   class Basketball_Team{
     public:
+    //declare constructor(s)
     //declare all setters and getters
+    //ALSO REMEMBER ALL GET FUNCTIONS MUST BE DECLARED CONST
 
-    double calculateWinrate(double gamesWOns, double gamesPlayed); //this is a function
-    void printChanceAtChampionship(); //this is a function
+
+    //this is a function that calculates cumulative winRate from the years 2021, 2020, 2019
+    double calculateWinrate(double gamesWOns, double gamesPlayed); 
+    
+    //this is a function that prints teamName, weightedScore, winRate, thisTeamsChances. 
+    //be sure to convert thisTeamsChances into a string by calling the function toStr_yourChampionshipChance 
+    void printStatistics(); 
     
     private:
     //declare variables in private fields
+    string teamName;
+    double weightedScore;
     double gamesWon;
     double gamesPlayed;
     double winRate;
-    yourChampionshipChance probableChance;
-
+    yourChampionshipChance thisTeamsChances;
+    
   }; //end of BasketBall_team class
   
 } //end of BASKETBALL_SPACE
