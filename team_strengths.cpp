4b5d4f6f7746f4514f9032e497e5bd1a4bfcbb5b
Kao Saephan <112367295+ksaephan@users.noreply.github.com>
2022-11-12 13:37:44 -0800

    Update team_strengths.cpp

--- team_strengths.cpp
+++ team_strengths.cpp
@@ -1,6 +1,20 @@
 /*
 Group Members: Andy Almeida, Kao Saephan, Nicholas Valencia, Umaran Ahmadzai, Shanil Prasad
-
 */
 
-//didnt get this far yet
+#include "basketball_team.h"
+#include "team_strengths.h"
+#include "championshipChance.h"
+
+namespace BASKETBALL_SPACE{
+  //implement all functions for class team_strengths
+  //REMEMBER ALL FUNCTIONS MUST HAVE SCOPE RESOLUTION OPERATOR WHICH ARE DOUBLE COLONS
+  //Format: Team_Strengths::someFunction()
+  
+  //ALSO REMEMBER ALL GET FUNCTIONS MUST BE DECLARED CONST
+  //Format: Team_Strengths::getFunction() const{ function body }
+  
+  //REMEMBER EVERYTHING MUST STAY INSIDE THE NAMESPACE BASKETBALL_SPACE
+  
+  
+} //end of BASKETBALL_SPACE
