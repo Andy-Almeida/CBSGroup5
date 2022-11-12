fc13d8def495e761c5bf549c4e6a313ef8bd2997
Kao Saephan <112367295+ksaephan@users.noreply.github.com>
2022-11-11 22:30:10 -0800

    Create championshipChance.cpp

---
+++ championshipChance.cpp
@@ -0,0 +1,27 @@
+/*
+Group Members: Andy Almeida, Kao Saephan, Nicholas Valencia, Umaran Ahmadzai, Shanil Prasad
+*/
+
+#include <string>
+#include "championshipChance.h"
+
+namespace BASKETBALL_SPACE{
+    using namespace std;
+
+    //this function simply turns the yourChampionshipChance enumerated class object into a string
+    string toStr_yourChampionshipChance( yourChampionshipChance chance ){
+        switch (chance){
+            case yourChampionshipChance::GreatChance:
+                return "Great Chance";
+            case yourChampionshipChance::GoodChance:
+                return "Good Chance";
+            case yourChampionshipChance::LowChance:
+                return "Low Chance";
+            case yourChampionshipChance::ExtremelyUnlikely:
+                return "Extremely Unlikely";
+            default:  //this will not happen. just to make this compiler not generate a warning.
+                return "Nobody Knows, this could be an error";  
+        } 
+    }//end of the function 
+
+} //end of BASKETBALL_SPACE
