ce0bd6090983cb1c8331c63fb194c00e04da5dd8
Kao Saephan <112367295+ksaephan@users.noreply.github.com>
2022-11-11 22:29:50 -0800

    Create championshipChance.h

---
+++ championshipChance.h
@@ -0,0 +1,16 @@
+/*
+Group Members: Andy Almeida, Kao Saephan, Nicholas Valencia, Umaran Ahmadzai, Shanil Prasad
+
+declares utility datatypes and functions to assign the championship chance to each team
+    in the namespace BASKETBALL_SPACE
+*/
+
+#ifndef championshipChance.h
+#define championshipChance.h
+
+namespace BASKETBALL_SPACE{
+
+enum class yourChampionshipChance {GreatChance, GoodChance, LowChance, ExtremelyUnlikely };
+
+} //end of namespace BASKETBALL SPACE
+#endif //end of championshipChance.h
