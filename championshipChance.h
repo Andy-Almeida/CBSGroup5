b77534f32271bcebd42c4264c7236da202328c8c
Kao Saephan <112367295+ksaephan@users.noreply.github.com>
2022-11-12 13:36:55 -0800

    Update championshipChance.h

--- championshipChance.h
+++ championshipChance.h
@@ -1,16 +1,27 @@
 /*
 Group Members: Andy Almeida, Kao Saephan, Nicholas Valencia, Umaran Ahmadzai, Shanil Prasad
 
+this file contains all library calls
+
 declares utility datatypes and functions to assign the championship chance to each team
     in the namespace BASKETBALL_SPACE
 */
 
-#ifndef championshipChance.h
-#define championshipChance.h
+#ifndef championshipChance_h
+#define championshipChance_h
+
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include "basketball_team.h"
+#include "team_strengths.h"
 
 namespace BASKETBALL_SPACE{
 
 enum class yourChampionshipChance {GreatChance, GoodChance, LowChance, ExtremelyUnlikely };
 
 } //end of namespace BASKETBALL SPACE
+
 #endif //end of championshipChance.h
