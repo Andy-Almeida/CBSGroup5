0afae96705a0cfaa8fb2b738e10995842774c747
Kao Saephan <112367295+ksaephan@users.noreply.github.com>
2022-11-11 20:53:14 -0800

    Update and rename basketballClass.h to basketball_team.h

---
+++ basketball_team.h
@@ -0,0 +1,27 @@
+/*
+declaration of the following class in namespace BASKETBALL_SPACE:
+  -class basketball_team
+  
+*/
+
+#ifndef basketball_team_h
+#define basketball_team_h
+
+namespace BASKETBALL_SPACE{
+  using namespace std;
+  
+  class basketball_team{
+    public:
+    //declare all setters and getters
+    //declare a function prototype called "calculateWinrate"
+    
+    private:
+    //declare three private fields
+    //games won
+    //games played
+    //winrate
+  };
+  
+} //end of BASKETBALL_SPACE
+
+#endif //end of basketball_team_h 
