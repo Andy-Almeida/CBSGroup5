2eb64dfd23dc126d104edf0639d02d24fa78541a
Kao Saephan <112367295+ksaephan@users.noreply.github.com>
2022-11-11 21:01:58 -0800

    Update basketball_team.cpp

--- basketball_team.cpp
+++ basketball_team.cpp
@@ -1,1 +1,18 @@
-//implementation phase
+/*
+Team Members:
+*/
+
+#include "basketball_team.h"
+
+namespace BASKETBALL_SPACE{
+  //implement all functions for class basketball_team
+  //REMEMBER ALL FUNCTIONS MUST HAVE SCOPE RESOLUTION OPERATOR WHICH ARE DOUBLE COLONS
+  //Format: basketball_team::someFunction()
+  
+  //ALSO REMEMBER ALL GET FUNCTIONS MUST BE DECLARED CONST
+  //Format: basketball_team::getFunction() const{ function body }
+  
+  //REMEMBER EVERYTHING MUST STAY INSIDE THE NAMESPACE BASKETBALL_SPACE
+  
+  
+} //end of BASKETBALL_SPACE
