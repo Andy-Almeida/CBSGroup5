d394889033b03b324bed3f8ee0a0a152907823e1
Kao Saephan <112367295+ksaephan@users.noreply.github.com>
2022-11-14 23:09:34 -0800

    contains some test cases for basketball_team

--- basketball_Main.cpp
+++ basketball_Main.cpp
@@ -5,11 +5,19 @@ Group Members: Andy Almeida, Kao Saephan, Nicholas Valencia, Umaran Ahmadzai, Sh
 
 
 #include "unittests.h"
+#include "basketball_team.h"
+#include "headers.h"
 
-using namespace std; 
+//using namespace std; 
+using namespace BASKETBALL_SPACE;
 
 int main() {
-    
+    Basketball_Team Coders("theCoders", "codeConference", 99, 100, 101, 102 );
+     
+
+    cout << "the win rate is " << Coders.calculateWinrate(Coders.getGamesWon(), Coders.getGamesPlayed()) << endl;
+    cout << "the weighted score is " << Coders.calculateWeightedScore(Coders.getOffensePoints(), Coders.getDefensePoints()) << endl;
+    cout << "the championship chance is: " << toStr_yourChampionshipChance(Coders.calculateTeamsChances(Coders.calculateWeightedScore(Coders.getOffensePoints(), Coders.getDefensePoints()))) << endl;
 
     cout << "program runs fine :)" << endl;
     return 0; 
